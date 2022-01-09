ba36eb046f4f1dd60342d3c4ff558074798610dd
Makrand <makrandshingare@gmail.com>
2022-01-09 14:24:29 +0530

    Welcom in JDBC

---
+++ src/main/java/com/biz/jdbc/DataBaseDemo.java
@@ -0,0 +1,11 @@
+package com.biz.jdbc;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataBaseDemo {
+    public static void main(String[] args)  {
+        System.out.println("Welcome in JDBC");
+
+    }
+}

1778d6f392abe95b84f73a7883a43b0ea6928e17
Makrand <makrandshingare@gmail.com>
2022-01-09 14:25:51 +0530

    Added : Added connection to database

--- src/main/java/com/biz/jdbc/DataBaseDemo.java
+++ src/main/java/com/biz/jdbc/DataBaseDemo.java
@@ -5,7 +5,23 @@ import java.sql.SQLException;
 
 public class DataBaseDemo {
     public static void main(String[] args)  {
-        System.out.println("Welcome in JDBC");
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver loaded");
+        }
+
+        catch (ClassNotFoundException e) {
+            System.out.println("driver is not loaded");
+        }
+        String JDBCURL="jdbc:mysql://127.0.0.1:3308/payroll_service";
+        System.out.println("Database connected....");
+
+        try {
+            DriverManager.getConnection(JDBCURL,"root","root");
+        } catch (SQLException e) {
+            System.out.println("Database not connected..!!!!");
+        }
 
     }
 }
